59c8d713991ef6594c44639bea53cbe4f60253f1
Nitish8800 <nitishfea120784@gmail.com>
2022-06-19 02:07:20 +0530

    patch data

--- src/app.js
+++ src/app.js
@@ -33,6 +33,61 @@ app.post("/user", async (req, res) => {
   }
 });
 
+// Read the Data of registered Students
+
+app.get("/user", async (req, res) => {
+  try {
+    const studentsData = await users.find();
+    res.send(studentsData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Get the individual Student Data
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const studentData = await users.findById(req.params.id);
+    if (!studentData) {
+      return res.status(404).send("No Student Data Found");
+    }
+    res.send(studentData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// delete the Student Data
+
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const studentData = await users.findByIdAndDelete(req.params.id);
+    if (!studentData) {
+      return res.status(404).send("No Student Data Found");
+    }
+    res.send(studentData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// update the Student Data
+
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const studentData = await users.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!studentData) {
+      return res.status(404).send("No Student Data Found");
+    }
+    res.send(studentData);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
